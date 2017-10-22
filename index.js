184997724f6e984bf8b41222f4597c22fb063536
Allen Anderson <dande313@byu.net>
2017-10-22 22:20:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const app = "I don't do much.";
+
+const token = '0d04c4bb5362c8552941320edd84f38f5cd21671'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
